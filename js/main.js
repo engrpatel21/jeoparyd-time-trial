eb33f9a579dac462020fc8ee4bcb72b41192a595
pratik <engrpatel21@gmail.com>
2020-08-07 20:40:07 -0400

    refactored fetch function to work with MVC model

--- js/main.js
+++ js/main.js
@@ -1,4 +1,57 @@
 
+
+
+let GameData = (function () {
+    
+    // hard coded categories to create options for html select element
+    const hardCodedCategories = [['Potpourriiii', 306], ['Stupid Answers', 136], ['Sports', 42], ['American History', 780], ['Animals', 21], ['3 Letter Words', 105], ['Science', 25], ['Transportation', 103], ['U.S. Cities', 7], ['People', 442], ['Television', 67], ['Hodgepodge', 227], ['State Capitals', 109], ['History', 114], ['The Bible', 31], ['Business & Industry', 31], ['U.S. Geography', 582], ['Annual Events', 1114], ['Common Bonds', 508], ['Food', 49], ['Rhyme Time', 561], ['Word Origins', 223], ['Pop Music', 770], ['Holidays & Observances', 662], ['Americana', 313], ['Food & Drink', 253], ['Weights & Measures', 420], ['Potent Potables', 83], ['Musical Instruments', 184], ['Bodies Of Water', 221], ['4 Letter Words', 51], ['Museums', 531], ['Nature', 267], ['Organizations', 357], ['World History', 530], ['Travel & Tourism', 369], ['Colleges & Universities', 672], ['Nonfiction', 793], ['World Capitals', 78], ['Literature', 574], ['Fruits & Vegetables', 777], ['Mythology', 680], ['U.S. History', 50], ['Religion', 99], ['The Movies', 309], ['First Ladies', 40], ['Fashion', 26], ['Homophones', 249], ['Quotations', 1420], ['Science & Nature', 218], ['Foreign Words & Phrases', 1145], ['Around The World', 1079], ['5 Letter Words', 139], ['Double Talk', 89], ['U.S. States', 17], ['Books & Authors', 197], ['Nursery Rhymes', 37], ['Brand Names', 2537], ['Familiar Phrases', 705], ['Before & After', 1800], ['Body Language', 897], ['Number, Please', 1195], ['The Old Testament', 128]]
+
+    
+
+    // game variables that will be manipulated by the game controller
+    let gameVar = {
+        pickedCategories: [null, null, null, null, null, null], //categories that were picked
+        questions: [[],[],[],[],[],[]],//each nested array represents 1 of 6 categories to choose from
+        playerNames: [], // player names
+        playerScores: [], //player scores
+        questionTimer: null // timer variable 
+    }
+    return {
+
+        getQuestionsData: function (idx, id, category_id, title, question, answer, invalid) {
+            gameVar.questions[idx].push({
+                questionID: id,
+                categoryID: category_id,
+                category: title,
+                question: question,
+                answer: answer,
+                invalid: invalid
+            })
+            gameVar.pickedCategories[idx] = title
+           
+            
+        },
+        emptyData: function (idx) {
+            gameVar.questions[idx] = []
+        },
+        
+        // function to return the hard coded categories to be used by the app controller
+        hardCodedCategories: function () {
+            return hardCodedCategories
+        },
+
+        gameVars: function () {
+            return gameVar
+        },
+        print: function () {
+            console.log(gameVar.questions)
+        }
+    }
+
+ })();
+
+
+
 let GameUI = (function () { 
 
     // an object for all the needed cached elements for dom minipulation
@@ -34,62 +87,6 @@ let GameUI = (function () {
 })();
 
 
-let GameData = (function (gUI) {
-    
-    // hard coded categories to create options for html select element
-    const hardCodedCategories = [['Potpourriiii', 306], ['Stupid Answers', 136], ['Sports', 42], ['American History', 780], ['Animals', 21], ['3 Letter Words', 105], ['Science', 25], ['Transportation', 103], ['U.S. Cities', 7], ['People', 442], ['Television', 67], ['Hodgepodge', 227], ['State Capitals', 109], ['History', 114], ['The Bible', 31], ['Business & Industry', 31], ['U.S. Geography', 582], ['Annual Events', 1114], ['Common Bonds', 508], ['Food', 49], ['Rhyme Time', 561], ['Word Origins', 223], ['Pop Music', 770], ['Holidays & Observances', 662], ['Americana', 313], ['Food & Drink', 253], ['Weights & Measures', 420], ['Potent Potables', 83], ['Musical Instruments', 184], ['Bodies Of Water', 221], ['4 Letter Words', 51], ['Museums', 531], ['Nature', 267], ['Organizations', 357], ['World History', 530], ['Travel & Tourism', 369], ['Colleges & Universities', 672], ['Nonfiction', 793], ['World Capitals', 78], ['Literature', 574], ['Fruits & Vegetables', 777], ['Mythology', 680], ['U.S. History', 50], ['Religion', 99], ['The Movies', 309], ['First Ladies', 40], ['Fashion', 26], ['Homophones', 249], ['Quotations', 1420], ['Science & Nature', 218], ['Foreign Words & Phrases', 1145], ['Around The World', 1079], ['5 Letter Words', 139], ['Double Talk', 89], ['U.S. States', 17], ['Books & Authors', 197], ['Nursery Rhymes', 37], ['Brand Names', 2537], ['Familiar Phrases', 705], ['Before & After', 1800], ['Body Language', 897], ['Number, Please', 1195], ['The Old Testament', 128]]
-
-    const refs = gUI.refs()
-
-    // game variables that will be manipulated by the game controller
-    let gameVar = {
-        pickedCategories: [null, null, null, null, null, null], //categories that were picked
-        questions: [[],[],[],[],[],[]],//each nested array represents 1 of 6 categories to choose from
-        playerNames: [], // player names
-        playerScores: [], //player scores
-        questionTimer: null // timer variable 
-    }
-    return {
-        // this function will fetch all the data and store them in the questions 
-        fetchCategory: function (categoryID,category) {
-            fetch(`http://jservice.io/api/category?id=${categoryID}`)
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                gameVar.questions[category] = []
-                data.clues.forEach(i => {
-                    gameVar.questions[category].push({
-                        questionID: i.id,
-                        categoryID: i.category_id,
-                        category: data.title,
-                        question: i.question,
-                        answer: i.answer
-                    })
-                })
-                gameVar.pickedCategories[category] = data.title
-                refs.categories[category].textContent = gameVar.pickedCategories[category]
-                console.log(gameVar.questions)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        },
-        // function to return the hard coded categories to be used by the app controller
-        hardCodedCategories: function () {
-            return hardCodedCategories
-        },
-
-        gameVars: function () {
-            return gameVar
-        }
-    }
-
- })(GameUI);
-
-
-
-
 
 let GameController = (function (gD, gUI) { 
     
@@ -102,10 +99,31 @@ let GameController = (function (gD, gUI) {
     // cached references pulled from the gameUI
     const refs = gUI.refs();
     
-    console.log(refs.category)
 
     // sets the options for all the select elements using the hard coded categories
     refs.categorySelector.forEach(i => gUI.addSelection(hardCodedCategories, i))
+
+    function fetchCategory(categoryID, id) {
+        fetch(`http://jservice.io/api/category?id=${categoryID}`)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            gD.emptyData(id)
+            data.clues.forEach(i => {
+              
+                gD.getQuestionsData(id, i.id, i.category_id, data.title, i.question, i.answer, i.invalid_count)
+                // function (idx, id, category_id, title, question, answer, invalid)
+            })
+
+            refs.categories[id].textContent = data.title
+            console.log(gameVars.pickedCategories)
+            gD.print()
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
 
     // this function runs all the event listeners 
     function setupEvents() {
@@ -119,7 +137,7 @@ let GameController = (function (gD, gUI) {
         refs.categorySelector.forEach(i => i.onclick = (e) => {
             
             // fetches data from the api depending on the user selected category
-            gD.fetchCategory(e.target.value, e.target.id[3])
+            fetchCategory(e.target.value, e.target.id[3])
             
         })
     }
