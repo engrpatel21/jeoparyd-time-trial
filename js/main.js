15b1bbdc4357a4111250041d3fe36c90dc444264
pratik <engrpatel21@gmail.com>
2020-08-10 08:47:38 -0400

    fixed all undefined errors

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@
 let GameData = (function () {
     
     // hard coded categories to create options for html select element
-    const hardCodedCategories = [['Potpourriiii', 306], ['Stupid Answers', 136], ['Sports', 42], ['American History', 780], ['Animals', 21], ['3 Letter Words', 105], ['Science', 25], ['Transportation', 103], ['U.S. Cities', 7], ['People', 442], ['Television', 67], ['Hodgepodge', 227], ['State Capitals', 109], ['History', 114], ['The Bible', 31], ['Business & Industry', 176], ['U.S. Geography', 582], ['Annual Events', 1114], ['Common Bonds', 508], ['Food', 49], ['Rhyme Time', 561], ['Word Origins', 223], ['Holidays & Observances', 622], ['Americana', 313], ['Food & Drink', 253], ['Weights & Measures', 420], ['Potent Potables', 83], ['Musical Instruments', 184], ['Bodies Of Water', 211], ['4 Letter Words', 51], ['Museums', 539], ['Nature', 267], ['Organizations', 357], ['World History', 530], ['Travel & Tourism', 369], ['Colleges & Universities', 672], ['Nonfiction', 793], ['World Capitals', 78], ['Literature', 574], ['Fruits & Vegetables', 777], ['Mythology', 680], ['U.S. History', 50], ['Religion', 99], ['The Movies', 309], ['First Ladies', 41], ['Fashion', 26], ['Homophones', 249], ['Quotations', 1420], ['Science & Nature', 218], ['Foreign Words & Phrases', 1145], ['Around The World', 1079], ['5 Letter Words', 139], ['Double Talk', 89], ['U.S. States', 17], ['Books & Authors', 197], ['Nursery Rhymes', 37], ['Brand Names', 2537], ['Familiar Phrases', 705], ['Before & After', 1800], ['Body Language', 897], ['Number, Please', 1195], ['The Old Testament', 128]]
+    const hardCodedCategories = [[,-1],['Potpourriiii', 306], ['Stupid Answers', 136], ['Sports', 42], ['American History', 780], ['Animals', 21], ['3 Letter Words', 105], ['Science', 25], ['Transportation', 103], ['U.S. Cities', 7], ['People', 442], ['Television', 67], ['Hodgepodge', 227], ['State Capitals', 109], ['History', 114], ['The Bible', 31], ['Business & Industry', 176], ['U.S. Geography', 582], ['Annual Events', 1114], ['Common Bonds', 508], ['Food', 49], ['Rhyme Time', 561], ['Word Origins', 223], ['Holidays & Observances', 622], ['Americana', 313], ['Food & Drink', 253], ['Weights & Measures', 420], ['Potent Potables', 83], ['Musical Instruments', 184], ['Bodies Of Water', 211], ['4 Letter Words', 51], ['Museums', 539], ['Nature', 267], ['Organizations', 357], ['World History', 530], ['Travel & Tourism', 369], ['Colleges & Universities', 672], ['Nonfiction', 793], ['World Capitals', 78], ['Literature', 574], ['Fruits & Vegetables', 777], ['Mythology', 680], ['U.S. History', 50], ['Religion', 99], ['The Movies', 309], ['First Ladies', 41], ['Fashion', 26], ['Homophones', 249], ['Quotations', 1420], ['Science & Nature', 218], ['Foreign Words & Phrases', 1145], ['Around The World', 1079], ['5 Letter Words', 139], ['Double Talk', 89], ['U.S. States', 17], ['Books & Authors', 197], ['Nursery Rhymes', 37], ['Brand Names', 2537], ['Familiar Phrases', 705], ['Before & After', 1800], ['Body Language', 897], ['Number, Please', 1195], ['The Old Testament', 128]]
 
     
 
     // game variables that will be manipulated by the game controller
     let gameVar = {
-        pickedCategories: [null, null, null, null, null, null], //categories that were picked
+        pickedCategories: [], //categories that were picked
         questions: [[],[],[],[],[],[]],//each nested array represents 1 of 6 categories to choose from
         playerNames: [], // player names
         playerScores: [], //player scores
@@ -65,7 +65,7 @@ let GameData = (function () {
             })
         },
         generateRandomQs: function (categories, questions) {
-            let qObj = {}
+            let qObj = []
             let commQsArr = []
             let prevCat = []
             for (let key in categories) {
@@ -111,6 +111,21 @@ let GameData = (function () {
              
                 gameVar.questions[idx][i].question = tempStr
             }
+        },
+        fixFormat: function (arr, idx) {
+            // console.log('format', arr)
+            // console.log('format', idx)
+            // console.log('format', arr[idx])
+            arr[idx] = arr[idx].split(' ')
+            for (let i = 0; i < arr[idx].length; i++){
+                if (arr[idx][i].includes('.')) {
+                    arr[idx][i] = arr[idx][i].toUpperCase()
+                } else {
+                    arr[idx][i] = arr[idx][i][0].toUpperCase() + arr[idx][i].slice(1)
+                }
+            }
+            arr[idx] = arr[idx].join(' ')
+         
         },
         cleanAnsewrs: function (idx) {
             let tempStr = ''
@@ -167,8 +182,12 @@ let GameUI = (function () {
         col2: document.querySelectorAll('.col2'),
         col3: document.querySelectorAll('.col3'),
         col4: document.querySelectorAll('.col4'),
-        col5: document.querySelectorAll('.col5')
+        col5: document.querySelectorAll('.col5'),
+        submit: document.getElementById('submit-btn'),
+        categorySelectorContainer: document.querySelector('.category-container'),
+        reset: document.getElementById('reset-btn')
     }
+
 
     // function that creates options to be used in html select element
     function newOps(arr) {
@@ -188,28 +207,36 @@ let GameUI = (function () {
         },
         renderInit: function (idx, categoryArray, questionsData, rGQuestions) {
             cachedRef.categories[idx].textContent = categoryArray[idx]
-        
-            cachedRef.col0.forEach((el, i) => {
-                el.textContent = questionsData[0][rGQuestions[0][i]].question
-            })
-            cachedRef.col1.forEach((el, i) => {
+            console.log('render', idx)
+            if (idx == 0) {
+                console.log(idx)
+                cachedRef.col0.forEach((el, i) => {
+                    el.textContent = questionsData[idx][rGQuestions[idx][i]].question
+                })
+            }else if (idx == 1) {
+                cachedRef.col1.forEach((el, i) => {
               
-                 el.textContent = questionsData[1][rGQuestions[1][i]].question
-            })
-            cachedRef.col2.forEach((el, i) => {
+                    el.textContent = questionsData[idx][rGQuestions[idx][i]].question
+                })
+            } else if (idx == 2) {
+                cachedRef.col2.forEach((el, i) => {
                 
-                 el.textContent = questionsData[2][rGQuestions[2][i]].question
-            })
-            cachedRef.col3.forEach((el, i) => {
-                el.textContent = questionsData[3][rGQuestions[3][i]].question
-            })
-            cachedRef.col4.forEach((el, i) => {
-                 el.textContent = questionsData[4][rGQuestions[4][i]].question
-            })
-            cachedRef.col5.forEach((el, i) => {
+                    el.textContent = questionsData[idx][rGQuestions[idx][i]].question
+                })
+            }else if (idx == 3) {
+                cachedRef.col3.forEach((el, i) => {
+                    el.textContent = questionsData[idx][rGQuestions[idx][i]].question
+                })
+            }else if (idx == 4) {
+                cachedRef.col4.forEach((el, i) => {
+                    el.textContent = questionsData[idx][rGQuestions[idx][i]].question
+                })
+            }else if (idx == 5) {
+                cachedRef.col5.forEach((el, i) => {
                 
-                el.textContent = questionsData[5][rGQuestions[5][i]].question
-            })
+                    el.textContent = questionsData[idx][rGQuestions[idx][i]].question
+                })
+            }
       
         }
 
@@ -235,35 +262,44 @@ let GameController = (function (gD, gUI) {
     refs.categorySelector.forEach(i => gUI.addSelection(hardCodedCategories, i))
 
     function fetchCategory(categoryID, idx) {
-        fetch(`http://jservice.io/api/category?id=${categoryID}`)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            gD.emptyData(idx)
-            data.clues.forEach(clue => {
-                if ((clue.question.length > 3 )
-                    && (!(clue.question.toLowerCase().includes('audio')) || !(clue.question.toLowerCase().includes('video')))) {
-                    gD.getQuestionsData(idx, clue.id, clue.category_id, data.title, clue.question, clue.answer, clue.invalid_count) 
-                } 
-            })
-            gD.cleanQuestions(idx, ')')
-            gD.cleanQuestions(idx, ']')
-            gD.cleanInvalidCharQs(idx)
-            gD.cleanAnsewrs(idx)
-            gameVars.checkedCategories = gD.checkCategories(gameVars.pickedCategories) 
-            gameVars.generatedQuestions = gD.generateRandomQs(gameVars.checkedCategories, gameVars.questions)
-            gD.finalClean(gameVars.questions, idx)
-            //console.log(gameVars.questions[idx].findIndex((el,i,arr) => arr[i].question === ""))
-            // console.log(gameVars.questions[idx][187])
+        console.log('begining of fetc',idx)
+            fetch(`http://jservice.io/api/category?id=${categoryID}`)
+                .then(response => {
+                    console.log('then',idx)
+                    // console.log(response)
+                    return response.json()
+                })
+                .then(data => {
+                    gD.emptyData(idx)
+                    data.clues.forEach(clue => {
+                        if ((clue.question.length > 3)
+                            && (!(clue.question.toLowerCase().includes('audio')) || !(clue.question.toLowerCase().includes('video')))) {
+                            gD.getQuestionsData(idx, clue.id, clue.category_id, data.title, clue.question, clue.answer, clue.invalid_count)
+                        }
+                    })
+                   
+                    gD.cleanQuestions(idx, ')')
+                    gD.cleanQuestions(idx, ']')
+                    gD.cleanInvalidCharQs(idx)
+                    gD.cleanAnsewrs(idx)
+                    gameVars.checkedCategories = gD.checkCategories(gameVars.pickedCategories)
+                    gameVars.generatedQuestions = gD.generateRandomQs(gameVars.checkedCategories, gameVars.questions)
+                    gD.finalClean(gameVars.questions, idx)
+                    // console.log(gameVars.questions)
+                    // console.log(gameVars.pickedCategories)
+                    // console.log(gameVars.generatedQuestions)
+                    //console.log(gameVars.questions[idx].findIndex((el,i,arr) => arr[i].question === ""))
+                    // console.log(gameVars.questions[idx][187])
            
-        })
-            .then(() => {
-                gUI.renderInit(idx, gameVars.pickedCategories, gameVars.questions, gameVars.generatedQuestions)
-         })
-         .catch(err => {
-             console.log(err)
-        })
+                })
+                .then(() => {
+                    gD.fixFormat(gameVars.pickedCategories, idx)
+                    gUI.renderInit(idx, gameVars.pickedCategories, gameVars.questions, gameVars.generatedQuestions)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        
     }
 
     // this function runs all the event listeners 
@@ -275,24 +311,43 @@ let GameController = (function (gD, gUI) {
         }
         
         // event listener for selecting categories 
-        refs.categorySelector.forEach(i => i.onclick = (e) => {
+        refs.categorySelector.forEach(category => category.onclick = (e) => {
             
+      
             // fetches data from the api depending on the user selected category
-            fetchCategory(e.target.value, e.target.id[3])
-
-          
-           
+            if (e.target.value != -1) { 
+                fetchCategory(e.target.value, e.target.id[3])
+            }
         })
+
+        refs.submit.onclick = (e) => {
+            refs.board.style.display = ''
+            refs.categorySelectorContainer.style.display = 'none'
+            refs.reset.style.display = ''
+            refs.submit.style.display = 'none'
+        }  
+        refs.reset.onclick = (e) => {
+            refs.board.style.display = 'none'
+            refs.categorySelectorContainer.style.display = ''
+            refs.submit.style.display = ''
+            refs.reset.style.display = 'none'
+            refs.categorySelector.forEach(category => {
+                category.selectedIndex = 0
+            })
+            gameVars.questions = [[], [], [], [], [], []]
+            gameVars.pickedCategories = []
+        }
     }
 
     return {
         // starts the game
         init: function () {
             console.log('start')
-            refs.categorySelector.forEach((category,idx) => {
-                fetchCategory(category.value, idx )
-            })
-            
+            // refs.categorySelector.forEach((category,idx) => {
+            //     fetchCategory(category.value, idx )
+            // })
+            refs.board.style.display = 'none'
+            refs.reset.style.display = 'none'
             setupEvents();
         }
     }
